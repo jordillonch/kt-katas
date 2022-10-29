0894cb9edf8bd3f8aa1c93e304224f94338f2ccf
Jordi Llonch <llonch.jordi@gmail.com>
2022-10-29 22:24:42 +0200

    refactor

--- colors/src/main/kotlin/org/katas/Colors.kt
+++ colors/src/main/kotlin/org/katas/Colors.kt
@@ -2,26 +2,21 @@ package org.katas
 
 @JvmInline
 value class Color(private val value: Int) {
-    fun redComponent(): Int = value shr 16
-    fun greenComponent(): Int = 0x00FF00 and value shr 8
-    fun blueComponent(): Int = 0x0000FF and value
+    fun red() = value shr 16
+    fun green() = 0x00FF00 and value shr 8
+    fun blue() = 0x0000FF and value
 }
 
 class Colors {
-    fun nearest(color: Color): Color {
-        return when {
-            (color.redComponent() > color.greenComponent()) and
-                    (color.redComponent() > color.blueComponent()) -> Color(0xFF0000)
-            color.greenComponent() > color.blueComponent() -> Color(0x00FF00)
-            else -> Color(0x0000FF)
-        }
+    fun nearest(color: Color): Color = when {
+        (color.red() > color.green()) && (color.red() > color.blue()) -> Color(0xFF0000)
+        color.green() > color.blue() -> Color(0x00FF00)
+        else -> Color(0x0000FF)
     }
-    fun farthest(color: Color): Color {
-        return when {
-            (color.redComponent() < color.greenComponent()) and
-                    (color.redComponent() < color.blueComponent()) -> Color(0xFF0000)
-            color.greenComponent() < color.blueComponent() -> Color(0x00FF00)
-            else -> Color(0x0000FF)
-        }
+
+    fun farthest(color: Color): Color = when {
+        (color.red() < color.green()) && (color.red() < color.blue()) -> Color(0xFF0000)
+        color.green() < color.blue() -> Color(0x00FF00)
+        else -> Color(0x0000FF)
     }
 }
