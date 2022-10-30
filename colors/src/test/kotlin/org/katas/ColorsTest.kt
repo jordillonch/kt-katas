e8579c8ce591af1fa49772fc461124490811855c
Jordi Llonch <llonch.jordi@gmail.com>
2022-10-30 15:39:41 +0100

    test refactor to use dynamic tests

--- colors/src/test/kotlin/org/katas/ColorsTest.kt
+++ colors/src/test/kotlin/org/katas/ColorsTest.kt
@@ -6,35 +6,35 @@ import io.kotest.matchers.shouldBe
 class ColorsTest : FunSpec({
     val colors = Colors()
 
-    test("Colors should be nearest to red") {
-        colors.nearest(Color(0xFF0000)) shouldBe Color(0xFF0000)
-        colors.nearest(Color(0xF40203)) shouldBe Color(0xFF0000)
-        colors.nearest(Color(0xF4F2F3)) shouldBe Color(0xFF0000)
+    listOf(Color(0xFF0000), Color(0x221100), Color(0xFFFEFD)).forEach {
+        test("$it should be nearest to red") {
+            colors.nearest(it) shouldBe Color(0xFF0000)
+        }
     }
-    test("Colors should be nearest to green") {
-        colors.nearest(Color(0x00FF00)) shouldBe Color(0x00FF00)
-        colors.nearest(Color(0x02F403)) shouldBe Color(0x00FF00)
-        colors.nearest(Color(0xF2F4F3)) shouldBe Color(0x00FF00)
+    listOf(Color(0x00FF00), Color(0x112200), Color(0xFEFFFD)).forEach {
+        test("$it should be nearest to green") {
+            colors.nearest(it) shouldBe Color(0x00FF00)
+        }
     }
-    test("Colors should be nearest to blue") {
-        colors.nearest(Color(0x0000FF)) shouldBe Color(0x0000FF)
-        colors.nearest(Color(0x0203F4)) shouldBe Color(0x0000FF)
-        colors.nearest(Color(0xF2F3F4)) shouldBe Color(0x0000FF)
+    listOf(Color(0x0000FF), Color(0x001122), Color(0xFEFDFF)).forEach {
+        test("$it should be nearest to blue") {
+            colors.nearest(it) shouldBe Color(0x0000FF)
+        }
     }
 
-    test("Colors should be farthest to red") {
-        colors.farthest(Color(0x0001FF)) shouldBe Color(0xFF0000)
-        colors.farthest(Color(0x0203F4)) shouldBe Color(0xFF0000)
-        colors.farthest(Color(0xF2F3F4)) shouldBe Color(0xFF0000)
+    listOf(Color(0x0001FF), Color(0x0203F4), Color(0xF2F3F4)).forEach {
+        test("$it should be farthest to red") {
+            colors.farthest(it) shouldBe Color(0xFF0000)
+        }
     }
-    test("Colors should be farthest to green") {
-        colors.farthest(Color(0x0100FF)) shouldBe Color(0x00FF00)
-        colors.farthest(Color(0xF40203)) shouldBe Color(0x00FF00)
-        colors.farthest(Color(0xF4F2F3)) shouldBe Color(0x00FF00)
+    listOf(Color(0x0100FF), Color(0xF40203), Color(0xF4F2F3)).forEach {
+        test("$it should be farthest to green") {
+            colors.farthest(it) shouldBe Color(0x00FF00)
+        }
     }
-    test("Colors should be farthest to blue") {
-        colors.farthest(Color(0xFF0100)) shouldBe Color(0x0000FF)
-        colors.farthest(Color(0x03F402)) shouldBe Color(0x0000FF)
-        colors.farthest(Color(0xF3F4F2)) shouldBe Color(0x0000FF)
+    listOf(Color(0xFF0100), Color(0x03F402), Color(0xF3F4F2)).forEach {
+        test("$it should be farthest to green") {
+            colors.farthest(it) shouldBe Color(0x0000FF)
+        }
     }
 })
