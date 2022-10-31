3c231a942a963f43a9c3d76e00d2fd1bc1568e90
Jordi Llonch <llonch.jordi@gmail.com>
2022-10-31 16:38:17 +0100

    minesweeper kata solution

--- minesweeper/src/main/kotlin/org/katas/Minesweeper.kt
+++ minesweeper/src/main/kotlin/org/katas/Minesweeper.kt
@@ -1,4 +1,54 @@
 package org.katas
 
+data class Board(val columns: Int, val rows: Int, val board: List<String>) {
+    val mineSymbol = '*'
+
+    fun isEmpty() = rows == 0 && columns == 0
+
+    fun analyze(): List<String> =
+        board.mapIndexed() { y, row ->
+            row.mapIndexed() { x, _ ->
+                if (isMine(x, y)) mineSymbol
+                else sumSurroundingMines(x, y).toString()
+            }.joinToString("")
+        }
+
+    private fun isMine(x: Int, y: Int): Boolean = when {
+        isOutOfBoardBoundaries(x, y) -> false
+        else -> board[y][x] == mineSymbol
+    }
+
+    private fun isOutOfBoardBoundaries(x: Int, y: Int) = (x < 0) or (y < 0) or (x >= columns) or (y >= rows)
+
+    private fun sumSurroundingMines(x: Int, y: Int): Int =
+        (x - 1..x + 1).fold(0) { accX, xPos ->
+            accX + (y - 1..y + 1).fold(0) { accY, yPos ->
+                accY + if (isMine(xPos, yPos)) 1 else 0
+            }
+        }
+}
+
+class Boards(private val input: String) {
+    fun toBoards(): List<Board> {
+        val inputLines = input.trim().split("\n")
+        return inputLines.mapIndexedNotNull() { index, line ->
+            val y = line.split(" ")[0].toIntOrNull()
+            if (y != null) {
+                val x = line.split(" ")[1].toInt()
+                Board(x, y, inputLines.subList(index + 1, index + 1 + y))
+            } else null
+        }
+    }
+}
+
 class Minesweeper {
+    fun play(input: String) =
+        Boards(input)
+            .toBoards()
+            .mapIndexed { index, inputBoard -> analyze(index, inputBoard) }
+            .joinToString("\n").trim()
+
+    private fun analyze(index: Int, board: Board) =
+        if (board.isEmpty()) ""
+        else "Field #${index + 1}:\n" + board.analyze().joinToString("\n") + "\n"
 }
--- minesweeper/src/test/kotlin/org/katas/MinesweeperTest.kt
+++ minesweeper/src/test/kotlin/org/katas/MinesweeperTest.kt
@@ -1,14 +1,36 @@
 package org.katas
 
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 
-class MinesweeperTest : FunSpec({
+class MinesweeperTest : ShouldSpec({
     val minesweeper = Minesweeper()
 
-    listOf(true).forEach {
-        test("$it should be true") {
-            it shouldBe true
-        }
+    should("play minesweeper") {
+        minesweeper.play(
+            """
+            4 4
+            *...
+            ....
+            .*..
+            ....
+            3 5
+            **...
+            .....
+            .*...
+            0 0
+            """.trimIndent()
+        ) shouldBe """
+            Field #1:
+            *100
+            2210
+            1*10
+            1110
+            
+            Field #2:
+            **100
+            33200
+            1*100
+            """.trimIndent()
     }
 })
