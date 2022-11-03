d89a27050179d7366e63244b905933eed22f764f
Jordi Llonch <llonch.jordi@gmail.com>
2022-11-03 17:04:46 +0100

    style

--- restaurant/src/main/kotlin/org/katas/Restaurant.kt
+++ restaurant/src/main/kotlin/org/katas/Restaurant.kt
@@ -2,10 +2,10 @@ package org.katas
 
 import java.util.UUID
 
-// size: number of chairs around this table
+/* size: number of chairs around this table */
 data class Table(val size: Int = 0, val id: UUID = UUID.randomUUID())
 
-// size: number of people in the group
+/* size: number of people in the group */
 data class CustomerGroup(val size: Int = 0, val id: UUID = UUID.randomUUID())
 
 data class TableState(
@@ -39,85 +39,50 @@ class SeatingManager(tables: List<Table>) {
     private val tablesState: MutableList<TableState> = tables.sortedBy { it.size }
         .map { TableState(it, mutableListOf()) }.toMutableList()
 
-    /*
-    * Group arrives and wants to be seated.
-    * Complexity: 2 * O(nTables)
-    *             nTables = the number of tables
-    */
+    /* Group arrives and wants to be seated.
+    * Complexity: 2 * O(numberOfTables) */
     fun arrives(group: CustomerGroup) {
         if (seat(group) == null) {
             waitingList.add(group)
         }
     }
 
-    /*
-    * Whether seated or not, the group leaves the restaurant.
-    * Complexity: O(nTables) * O(nGroupsInTable) + O(nGroupsInTable) + 2 * O(nTables) + O(nWaitingList)
-    *             nTables = the number of tables
-    *             nGroupsInTable = number of groups seated in a table
-    *             nWaitingList = the number of groups waiting to be seated
-    *
-    *             locateTableState: O(nTables) * O(nGroupsInTable)
-    *             locateTableState.free: O(nGroupsInTable)
-    *             tryToLocateTableForWaitingList: 2 * O(nTables) + O(nWaitingList)
-    */
+    /* Whether seated or not, the group leaves the restaurant.
+    *  Complexity: O(numberOfTables) * O(numberOfGroupsInATable) + O(numberOfGroupsInATable)
+    *              + 2 * O(numberOfTables) + O(groupsInTheWaitingList) */
     fun leaves(group: CustomerGroup) {
         locateTableState(group)?.free(group)
         waitingList.remove(group)
         tryToLocateTableForWaitingList()
     }
 
-    /*
-    * Return the table at which the group is seated, or null if they are not seated (whether they're waiting or
-    * already left).
-    * Complexity: O(nTables) * O(nGroupsInTable)
-    *             nTables = the number of tables
-    *             nGroupsInTable = number of groups seated in a table
-    */
+    /* Return the table at which the group is seated, or null if they are not seated (whether they're waiting or
+    *  already left).
+    *  Complexity: O(numberOfTables) * O(numberOfGroupsInATable) */
     fun locate(group: CustomerGroup): Table? = locateTableState(group)?.table
 
-    /*
-    * Complexity: O(nTables) * O(nGroupsInTable)
-    *             nTables = the number of tables
-    *             nGroupsInTable = number of groups seated in a table
-    */
+    /* Complexity: O(numberOfTables) * O(numberOfGroupsInATable) */
     private fun locateTableState(group: CustomerGroup) = tablesState.find { it.groups.contains(group) }
 
-    /*
-    * Complexity: 2 * O(nTables)
-    *             nTables = the number of tables
-    *             nGroupsInTable = number of groups seated in a table
-    *
-    *             locateFreeWholeTable = O(nTables)
-    *             locateFreeSharedTable = O(nTables)
-    */
+    /* Complexity: 2 * O(numberOfTables) */
     private fun seat(group: CustomerGroup): Table? {
         locateFreeWholeTable(group)?.occupy(group)?.let { return it.table }
         locateFreeSharedTable(group)?.occupy(group)?.let { return it.table }
         return null
     }
 
-    /*
-    * Complexity: 2 * O(nTables) + O(nWaitingList)
-    *             nWaitingList = the number of groups waiting to be seated
-    */
+    /* Complexity: 2 * O(numberOfTables) + O(groupsInTheWaitingList) */
     private fun tryToLocateTableForWaitingList() {
         waitingList
             .filter { seat(it) != null }
             .forEach { waitingList.remove(it) }
     }
 
-    /*
-    * Complexity: O(nTables)
-    *             nTables = the number of tables
-    */
+    /* Complexity: O(numberOfTables) */
     private fun locateFreeWholeTable(group: CustomerGroup): TableState? =
         tablesState.find { it.groups.size == 0 && it.table.size >= group.size }
 
-    /*
-    * Complexity: O(nTables)
-    *             nTables = the number of tables
-    */
+    /* Complexity: O(numberOfTables) */
     private fun locateFreeSharedTable(group: CustomerGroup): TableState? =
         tablesState.firstOrNull { it.isThereSpaceFor(group) }
 }
