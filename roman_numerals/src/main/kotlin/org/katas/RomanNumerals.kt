c15b5f15605da56c6a5e33568a359575c640cd64
Jordi Llonch <llonch.jordi@gmail.com>
2022-11-05 18:07:28 +0100

    roman numerals solution

--- roman_numerals/src/main/kotlin/org/katas/RomanNumerals.kt
+++ roman_numerals/src/main/kotlin/org/katas/RomanNumerals.kt
@@ -1,5 +1,43 @@
 package org.katas
 
 fun romanNumerals(number: Int): String {
-    return ""
+    val romanUnits = mapOf(
+        1000 to "M",
+        500 to "D",
+        100 to "C",
+        50 to "L",
+        10 to "X",
+        5 to "V",
+        1 to "I"
+    )
+    return romanUnits.entries
+        .fold(Pair(number, "")) { (rest, result), (romanNumeralInt, romanNumeral) ->
+            calculateRomanNumeralsUntil3Digits(rest, romanNumeralInt, romanNumeral)
+                .let { (rest, romanResult) ->
+                    calculateRomanNumeralsWhenSubtractingOne(romanResult, rest, romanNumeralInt, romanNumeral)
+                }.let { (rest, romanResult) ->
+                    Pair(rest, result + romanResult)
+                }
+        }.second
 }
+
+private fun calculateRomanNumeralsUntil3Digits(
+    number: Int,
+    romanNumeralInt: Int,
+    romanNumeral: String
+): Pair<Int, String> {
+    val timesOfRomanUnits = number / romanNumeralInt
+    return Pair(number - romanNumeralInt * timesOfRomanUnits, romanNumeral.repeat(timesOfRomanUnits))
+}
+
+private fun calculateRomanNumeralsWhenSubtractingOne(
+    result: String,
+    rest: Int,
+    romanNumeralInt: Int,
+    romanNumeral: String
+) =
+    if (rest != 0 && romanNumeralInt - 1 == rest) {
+        Pair(rest - romanNumeralInt + 1, result + "I" + romanNumeral)
+    } else {
+        Pair(rest, result)
+    }
--- roman_numerals/src/test/kotlin/org/katas/RomanNumeralsTest.kt
+++ roman_numerals/src/test/kotlin/org/katas/RomanNumeralsTest.kt
@@ -6,7 +6,41 @@ import io.kotest.matchers.shouldBe
 class RomanNumeralsTest : ShouldSpec({
     should("print roman numerals") {
         romanNumerals(1) shouldBe "I"
-        romanNumerals(10) shouldBe "X"
+        romanNumerals(2) shouldBe "II"
+        romanNumerals(3) shouldBe "III"
+        romanNumerals(4) shouldBe "IV"
+        romanNumerals(5) shouldBe "V"
+        romanNumerals(6) shouldBe "VI"
         romanNumerals(7) shouldBe "VII"
+        romanNumerals(8) shouldBe "VIII"
+        romanNumerals(9) shouldBe "IX"
+        romanNumerals(10) shouldBe "X"
+        romanNumerals(11) shouldBe "XI"
+        romanNumerals(12) shouldBe "XII"
+        romanNumerals(13) shouldBe "XIII"
+        romanNumerals(14) shouldBe "XIV"
+        romanNumerals(15) shouldBe "XV"
+        romanNumerals(16) shouldBe "XVI"
+        romanNumerals(17) shouldBe "XVII"
+        romanNumerals(18) shouldBe "XVIII"
+        romanNumerals(19) shouldBe "XIX"
+        romanNumerals(20) shouldBe "XX"
+    }
+
+    should("print high roman numerals") {
+        romanNumerals(49) shouldBe "IL"
+        romanNumerals(50) shouldBe "L"
+        romanNumerals(51) shouldBe "LI"
+        romanNumerals(99) shouldBe "IC"
+        romanNumerals(100) shouldBe "C"
+        romanNumerals(101) shouldBe "CI"
+        romanNumerals(499) shouldBe "ID"
+        romanNumerals(500) shouldBe "D"
+        romanNumerals(501) shouldBe "DI"
+        romanNumerals(999) shouldBe "IM"
+        romanNumerals(1000) shouldBe "M"
+        romanNumerals(1001) shouldBe "MI"
+        romanNumerals(2000) shouldBe "MM"
+        romanNumerals(3000) shouldBe "MMM"
     }
 })
