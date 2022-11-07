6a17afdb7df0b1a5c3871021697917c8641113a7
Jordi Llonch <llonch.jordi@gmail.com>
2022-11-07 15:31:49 +0100

    fix roman numeral

--- roman_numerals/src/main/kotlin/org/katas/RomanNumerals.kt
+++ roman_numerals/src/main/kotlin/org/katas/RomanNumerals.kt
@@ -1,33 +1,59 @@
 package org.katas
 
-private data class RomanNumeral(val number: Int, val numeral: String)
+class RomanNumerals {
+    private val number1000 = RomanNumeral(1000, "M")
+    private val number500 = RomanNumeral(500, "D")
+    private val number100 = RomanNumeral(100, "C")
+    private val number50 = RomanNumeral(50, "L")
+    private val number10 = RomanNumeral(10, "X")
+    private val number5 = RomanNumeral(5, "V")
+    private val number1 = RomanNumeral(1, "I")
+    private val number0 = RomanNumeral(0, "")
 
-fun romanNumerals(number: Int) =
-    listOf(
-        RomanNumeral(1000, "M"),
-        RomanNumeral(500, "D"),
-        RomanNumeral(100, "C"),
-        RomanNumeral(50, "L"),
-        RomanNumeral(10, "X"),
-        RomanNumeral(5, "V"),
-        RomanNumeral(1, "I")
-    ).fold(RomanNumeralsIterator(number, "")) { (rest, result), romanNumeral ->
-        calculateRomanNumeralsUntil3Digits(rest, romanNumeral).let { (rest, romanResult) ->
-            calculateRomanNumeralsWhenSubtractingOne(romanResult, rest, romanNumeral)
-        }.let { (rest, romanResult) -> RomanNumeralsIterator(rest, result + romanResult) }
-    }.result
-
-private data class RomanNumeralsIterator(val rest: Int, val result: String)
-
-private fun calculateRomanNumeralsUntil3Digits(number: Int, romanNumeral: RomanNumeral) =
-    RomanNumeralsIterator(
-        number - romanNumeral.number * (number / romanNumeral.number),
-        romanNumeral.numeral.repeat(number / romanNumeral.number)
+    private val romanNumeralConfigurations = listOf(
+        RomanNumeralConfiguration(romanNumeral = number1000, canBeSubstractedBy = number100),
+        RomanNumeralConfiguration(romanNumeral = number500, canBeSubstractedBy = number100),
+        RomanNumeralConfiguration(romanNumeral = number100, canBeSubstractedBy = number10),
+        RomanNumeralConfiguration(romanNumeral = number50, canBeSubstractedBy = number10),
+        RomanNumeralConfiguration(romanNumeral = number10, canBeSubstractedBy = number1),
+        RomanNumeralConfiguration(romanNumeral = number5, canBeSubstractedBy = number1),
+        RomanNumeralConfiguration(romanNumeral = number1, canBeSubstractedBy = number0)
     )
 
-private fun calculateRomanNumeralsWhenSubtractingOne(result: String, rest: Int, romanNumeral: RomanNumeral) =
-    if (rest != 0 && romanNumeral.number - 1 == rest) {
-        RomanNumeralsIterator(rest - romanNumeral.number + 1, result + "I" + romanNumeral.numeral)
-    } else {
-        RomanNumeralsIterator(rest, result)
+    fun toSymbols(number: Int) =
+        romanNumeralConfigurations.fold(RomanNumeralsIterator(number, "")) { (rest, result), romanNumeral ->
+            romanNumeral.calculateRomanNumeralsUntil3Digits(rest).let { (rest, romanResult) ->
+                romanNumeral.calculateRomanNumeralsWhenSubtracting(romanResult, rest)
+            }.let { (rest, romanResult) -> RomanNumeralsIterator(rest, result + romanResult) }
+        }.result
+
+    private data class RomanNumeralsIterator(val rest: Int, val result: String)
+
+    private data class RomanNumeral(val number: Int, val symbol: String)
+
+    private data class RomanNumeralConfiguration(val romanNumeral: RomanNumeral, val canBeSubstractedBy: RomanNumeral) {
+        fun calculateRomanNumeralsUntil3Digits(number: Int): RomanNumeralsIterator {
+            val repetitions = number / romanNumeral.number
+            return if (repetitions <= 3) {
+                RomanNumeralsIterator(
+                    number - romanNumeral.number * repetitions,
+                    romanNumeral.symbol.repeat(repetitions)
+                )
+            } else {
+                RomanNumeralsIterator(number, "")
+            }
+        }
+
+        fun calculateRomanNumeralsWhenSubtracting(result: String, rest: Int): RomanNumeralsIterator {
+            val numberToSubtract = romanNumeral.number - canBeSubstractedBy.number
+            return if (rest != 0 && numberToSubtract == rest - (rest % numberToSubtract)) {
+                RomanNumeralsIterator(
+                    rest - numberToSubtract,
+                    result + canBeSubstractedBy.symbol + romanNumeral.symbol
+                )
+            } else {
+                RomanNumeralsIterator(rest, result)
+            }
+        }
     }
+}
